42eb377b4b32ee21e0a0df655cf70d2e4ccd84a6
Myunghyun Jung <kiparang87@hanmail.net>
2022-02-14 14:54:25 +0900

    commit_220214_3

---
+++ covid_analysis_2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('covid-variants.csv', index_col='location')
+
+df_1 = df.loc['Angola']
+
+# print(df_1['date'].drop_duplicates())
+df_2 = pd.DataFrame({})
+df_2['date'] = df_1['date'].drop_duplicates()
+# print(df_2)
+
+new_list = []
+
+for date in df_2['date']:
+    mark = (df_1['date'] == date)
+    df_1_new = df_1[mark]
+    num_sequences_sum = df_1_new['num_sequences'].sum()
+    new_list.append(num_sequences_sum)
+
+df_2['num_sequences'] = new_list
+print(df_2)
+
+plt.figure(figsize=(10, 10))
+plt.plot(df_2['date'], df_2['num_sequences'])
+
+plt.xticks(rotation='vertical')
+
+plt.title('Angola')
+plt.legend(labels=['Angola'], loc='best')
+
+plt.show()
