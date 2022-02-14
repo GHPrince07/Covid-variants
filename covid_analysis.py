ce830553c4411c4f77f16af61853b531546f96cf
Myunghyun Jung <kiparang87@hanmail.net>
2022-02-14 14:22:41 +0900

    commit_220214_2

--- covid_analysis.py
+++ covid_analysis.py
@@ -3,15 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('covid-variants.csv', index_col='location')
-# print(df)
-# print(df.location.drop_duplicates())
-# mask1 = (df['location'] =='Angola')
-# mask2 = (df['location'] =='Argentina')
-# df_1 = df[mask1]
-# df_2 = df[mask2]
-# print(df_1)
-# print()
-# print(df_2)
+
 df_1 = df.loc['Angola']
 
 plt.figure(figsize=(10, 10))
